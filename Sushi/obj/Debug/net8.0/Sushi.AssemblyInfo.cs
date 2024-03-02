4ef054e53e820d1a9408e67d8ef9c968718d2ac7
Dimka122 <kassindima1@gmail.com>
2024-03-02 07:24:07 +0200

    ProductController

--- Sushi/obj/Debug/net8.0/Sushi.AssemblyInfo.cs
+++ Sushi/obj/Debug/net8.0/Sushi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sushi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c0d3a3c8203c67a1666f8f046675c7e83abe08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5546d6ec86b6912b41826dff3eaac57681c2f022")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sushi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sushi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
