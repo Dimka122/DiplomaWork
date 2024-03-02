77c0d3a3c8203c67a1666f8f046675c7e83abe08
Dimka122 <kassindima1@gmail.com>
2024-03-02 06:53:47 +0200

    ConnectionString

--- Sushi/Program.cs
+++ Sushi/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Sushi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<VDbContext>(options =>
+                    options.UseSqlServer("DefaultConnection"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
