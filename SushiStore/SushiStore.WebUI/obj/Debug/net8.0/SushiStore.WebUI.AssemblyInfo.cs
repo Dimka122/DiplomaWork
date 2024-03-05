b09d485708839d016882016ea7d47336a2fd3da7
Dimka122 <kassindima1@gmail.com>
2024-03-05 06:10:21 +0200

    установим NuGet

--- SushiStore/SushiStore.WebUI/obj/Debug/net8.0/SushiStore.WebUI.AssemblyInfo.cs
+++ SushiStore/SushiStore.WebUI/obj/Debug/net8.0/SushiStore.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SushiStore.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e85bde38e78e13a49f9f2e9b50019ce0c28e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62001842f4d7404023fdac0db138aa9839a68d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SushiStore.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SushiStore.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
