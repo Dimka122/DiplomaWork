a236d7bf6700bee66546dd4e70c2d2a5fe96b9ee
Dimka122 <kassindima1@gmail.com>
2024-03-05 09:40:57 +0200

    создание модели Sushi

---
+++ SushiStore/SushiStore.Domain/Entities/Sushi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SushiStore.Domain.Entities
+{
+    public class Sushi
+    {
+        public int SushiId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
