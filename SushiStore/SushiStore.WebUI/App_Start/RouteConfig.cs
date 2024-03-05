6a021f6681ba914c95387d473dec470c7628de16
Dimka122 <kassindima1@gmail.com>
2024-03-05 11:03:26 +0200

    EntityFramework добавление NuGet

--- SushiStore/SushiStore.WebUI/App_Start/RouteConfig.cs
+++ SushiStore/SushiStore.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SushiStore.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Sushi", action = "List", id = UrlParameter.Optional }
             );
         }
     }
