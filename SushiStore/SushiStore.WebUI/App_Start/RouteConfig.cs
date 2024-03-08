89f634ae6093bda6d694f1cba2caed83ead5dd6d
Dimka122 <kassindima1@gmail.com>
2024-03-08 20:05:13 +0200

    Улучшение URL

--- SushiStore/SushiStore.WebUI/App_Start/RouteConfig.cs
+++ SushiStore/SushiStore.WebUI/App_Start/RouteConfig.cs
@@ -12,6 +12,11 @@ namespace SushiStore.WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Sushi", action = "List" }
+            );
 
             routes.MapRoute(
                 name: "Default",
