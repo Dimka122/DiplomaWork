6a10fca36c33ed85ca507334185422fd97d6ec90
Dimka122 <kassindima1@gmail.com>
2024-03-09 10:31:03 +0200

    Создание контроллера навигации

---
+++ SushiStore/SushiStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SushiStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        public string Menu()
+        {
+            return "Тестируем контроллер Nav";
+        }
+    }
+}
