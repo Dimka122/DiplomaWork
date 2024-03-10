9a8ad2bf5af8ac4ed45c624cdb42c0d90fac06d1
Dimka122 <kassindima1@gmail.com>
2024-03-10 10:19:31 +0200

    создание CartController

---
+++ SushiStore/SushiStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using SushiStore.Domain.Abstract;
+using SushiStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SushiStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private ISushiRepository repository;
+        public CartController(ISushiRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int sushiId, string returnUrl)
+        {
+            Sushi sushi = repository.Sushis
+                .FirstOrDefault(g => g.SushiId == sushiId);
+
+            if (sushi != null)
+            {
+                GetCart().AddItem(sushi, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int sushiId, string returnUrl)
+        {
+            Sushi sushi = repository.Sushis
+                .FirstOrDefault(g => g.SushiId == sushiId);
+
+            if (sushi != null)
+            {
+                GetCart().RemoveLine(sushi);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
