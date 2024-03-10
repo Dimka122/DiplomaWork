978e6b87ed4f80d969d0d1e4b6760f4ff4f2d899
Dimka122 <kassindima1@gmail.com>
2024-03-10 09:03:17 +0200

    Создание корзины тестового метода

---
+++ SushiStore/SushiStore.Domain/Entities/Cart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SushiStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public void AddItem(Sushi sushi,int quantity)
+        {
+            CartLine line=lineCollection
+                .Where(g=>g.Sushi.SushiId==sushi.SushiId)
+                .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Sushi = sushi,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(Sushi sushi)
+        {
+            lineCollection.RemoveAll(l=>l.Sushi.SushiId == sushi.SushiId);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Sushi.Price * e.Quantity);
+        }
+        public void Clear()
+        { lineCollection.Clear(); }
+        public IEnumerable<CartLine> Lines { get { return lineCollection; } }
+
+
+        public class CartLine
+        {
+            Sushi Sushi { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ SushiStore/SushiStore.UnitTests/CartTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SushiStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+       public void Can_Add_New_Lines()
+       {
+
+       }
+    }
+}
