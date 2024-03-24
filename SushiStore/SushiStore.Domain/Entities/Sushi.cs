73af17c27b721decf5e44dacd12c16eb49d72b39
Dimka122 <kassindima1@gmail.com>
2024-03-24 10:26:05 +0200

    метаданные

--- SushiStore/SushiStore.Domain/Entities/Sushi.cs
+++ SushiStore/SushiStore.Domain/Entities/Sushi.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SushiStore.Domain.Entities
 {
     public class Sushi
     {
+        [HiddenInput(DisplayValue = false)]
         public int SushiId { get; set; }
+
+        [Display(Name = "Название")]
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Описание")]
         public string Description { get; set; }
+
+        [Display(Name = "Категория")]
         public string Category { get; set; }
+
+        [Display(Name = "Цена (грн.)")]
         public decimal Price { get; set; }
     }
 }
